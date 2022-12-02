38247fc5196febe5ff3d68a3e17bce8bd28d739a
Brian McKenna <brianmckennaman@gmail.com>
2022-12-01 19:33:14 -0500

    created new pages folder and moved files into it

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,38 +1,20 @@
 import './App.css';
 import '../src'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import AboutMe from "./components/about"
+import AboutMe from "./pages/about"
 import Navbar from "./components/navbar"
 import Footer from "./components/footer"
 import Header from "./components/header"
-import Resume from "./components/resume"
-import Contact from "./components/contact"
-import Apps from "./components/apps"
+import Resume from "./pages/resume"
+import Contact from "./pages/contact"
+import Apps from "./pages/apps"
 function App() {
-
-  // var arr = [
-  //   {
-  //     name: "Millie",
-  //   },
-  //   {
-  //     name: "David",
-  //   },
-  //   {
-  //     name: "Roland",
-  //   },
-  //   {
-  //     name: "Sarah",
-  //   },
-  // ]
 
   return (
     <Router>
       <div className="App">
         <Header />
         <Navbar />
-        {/* {arr.map(item => {
-        return <AboutMe mops={item.name} />
-      })} */}
         <Routes>
           <Route exact path='/' element={<AboutMe />}>
           </Route>
