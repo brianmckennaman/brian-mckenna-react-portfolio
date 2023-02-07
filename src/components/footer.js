8a762880e8f8c35fddd93c56a9d1d6326580cf7c
Brian McKenna <brianmckennaman@gmail.com>
2023-02-06 22:42:45 -0500

    updated footer with link images

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,23 @@
+import githubPicture from '../assets/github.png'
+import linkedinPicture from '../assets/linkedin.png'
+import twitterPicture from '../assets/twitter.webp'
+import twitchPicture from '../assets/twitch.png'
 function Footer() {
 
     var style = {
-        footer: { backgroundColor: "lightBlue", color: "black", display: "flex", justifyContent: "center", height: "9vw"},
+        footer: { backgroundColor: "lightBlue", color: "black", display: "flex", justifyContent: "center", height: "10%" },
         footerText: { font: "Sans-Serif", display: "inline", marginLeft: "2vw", marginRight: "2vw", color: "blue", fontSize: "3vw", textDecoration: "none" },
-        linkContainer: {}
+        linkContainer: { display: "flex", justifyContent: "center" },
+        linkImage: { height: "5vw", width: "5vw"}
     }
     return (
         <footer style={style.footer}>
-            
             <h1>Check out my other links!
-            <div >
-                <p style={style.footerText}>
-                    <a style={style.footerText} href="https://github.com/brianmckennaman">Github</a>
-                </p>
-                <p style={style.footerText}>
-                    <a style={style.footerText} href="https://www.linkedin.com/in/brian-mckenna-a17b1b139/">Linkedin</a>
-                </p>
-                <p style={style.footerText}>
-                    <a style={style.footerText} href="https://twitter.com/OverlordHype">Twitter</a>
-                </p>
+            <div style={style.linkContainer}>
+                <a href="https://github.com/brianmckennaman"><img style={style.linkImage} src={githubPicture}></img></a>
+                <a href="https://www.linkedin.com/in/brian-mckenna-a17b1b139/"><img style={style.linkImage} src={linkedinPicture}></img></a>
+                <a href='https://twitter.com/overlordhyperion'><img style={style.linkImage} src={twitterPicture}></img></a>
+                <a href='https://www.twitch.tv/overlordhyperion'><img style={style.linkImage} src={twitchPicture}></img></a>
             </div>
             </h1>
         </footer>
